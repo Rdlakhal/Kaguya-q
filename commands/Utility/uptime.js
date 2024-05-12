7a5f208d852054ab3d11f72f1d89003220770aed
Jehad110 <168698645+Jehad110@users.noreply.github.com>
2024-05-12 01:06:39 +0100

    Update uptime.js

--- commands/Utility/uptime.js
+++ commands/Utility/uptime.js
@@ -1,7 +1,7 @@
 import moment from "moment-timezone";
 
 export default {
-  name: "أوبتايم",
+  name: "تايم",
   author: "Kaguya Project",
   cooldowns: 60,
   description: "بيانات البوت",
@@ -14,7 +14,7 @@ export default {
     const hours = Math.floor(uptime / 3600);
     const minutes = Math.floor((uptime - (hours * 3600)) / 60);
     const seconds = Math.floor(uptime % 60);
-    const uptimeStr = `البوت كان شغالا منذ ${hours} ساعة ، ${minutes} دقيقة ، و ${seconds} ثانية`;
+    const uptimeStr = `«« البوت كان شغالا منذ ${hours} 𝑯 ساعة ، ${minutes} دقيقة 𝒆𝑴 ، و ${seconds} 𝑺 ثانية`;
 
     const threads = await api.getThreadList(99999, null, ['INBOX']);
 
@@ -29,10 +29,10 @@ export default {
       }
     });
 
-    const output = `🤖 |حالة البوت\n\n` +
-      `الوقت الحالي ⏰: ${currentTime},\n` +
-      `إجمالي عدد المستخدمين 🧿: ${userCount}\n` +
-      `إجمالي عدد المجموعات 🗝️: ${groupCount}\n\n` +
+    const output = `🤖 |حالة سيرفر 🌐\n\n` +
+      `الوقت الحالي ☑️: ${currentTime},\n` +
+      `إجمالي عدد المستخدمين 👥: ${userCount}\n` +
+      `إجمالي عدد المجموعات 〽️: ${groupCount}\n\n` +
       `${uptimeStr}`;
 
     api.sendMessage(output, event.threadID);
